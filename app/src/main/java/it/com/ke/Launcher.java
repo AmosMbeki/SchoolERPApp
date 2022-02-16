9a2376e969101f08ebff906b5c566cef1a55289b
AmosMbeki <amosmbeki@gmail.com>
2022-02-16 20:47:30 +0300

    Launcher activity implementation

---
+++ app/src/main/java/it/com/ke/Launcher.java
@@ -0,0 +1,66 @@
+package it.com.ke;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class Launcher extends AppCompatActivity {
+
+    private EditText myEmail;
+    private EditText myPassword;
+    private Button myLogin;
+    private String x_email, x_password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        myEmail =  findViewById(R.id.myemail);
+        myPassword = findViewById(R.id.mypassword);
+        myLogin = findViewById(R.id.btn_button);
+
+        myLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkIfBlank();
+            }
+        });
+    }
+
+    private void checkIfBlank() {
+        x_email = myEmail.getText().toString();
+        x_password = myPassword.getText().toString();
+
+        if(x_email.length() < 6){
+            new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText("Oops...")
+                    .setContentText("Email is wrong!")
+                    .show();
+
+            return;
+        }
+
+        if(x_password.length() < 6){
+            new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                    .setTitleText("Oops...")
+                    .setContentText("Password is wrong!")
+                    .show();
+
+            return;
+        }
+        
+        moveToHomePage();
+    }
+
+    private void moveToHomePage() {
+        Intent next = new Intent(Launcher.this, Home.class);
+        startActivity(next);
+    }
+}
